bca603d1d1a1ffd665f88e05cdcca92e32a3edba
Hristiyan Milev <124285133+HristiyanMilev@users.noreply.github.com>
2023-02-06 21:44:00 +0200

    Added Task Sqrt(x) with my algorithm

---
+++ Sqrt(x)/Task/Sqrt(x)/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace Sqrt_x_
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(MySqrt(4)); //Case with 8 , Case with 2147395600
+        }
+
+        public static int MySqrt(int x)
+        {
+            if(x == 1)
+            {
+                return x;
+            }
+            for (double i = 0; i <= x; i++)
+            {
+
+                if (i * i > x)
+                {
+                    return Convert.ToInt32(i - 1);
+                }
+            }
+            return 0;
+        }
+    }
+}
